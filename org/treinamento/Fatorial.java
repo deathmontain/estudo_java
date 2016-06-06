1a68dbc11b70dd633c628e5e8154f471e9486dc6
Jônatas William <jonatas.santos@aedu.com>
2016-06-06 20:22:35 -0300

    Acrescentado o valor do !0.

--- org/treinamento/Fatorial.java
+++ org/treinamento/Fatorial.java
@@ -4,10 +4,10 @@ public class Fatorial {
 
 	public static void main(String[] args) {
 		int fatorial = 1;
+		System.out.println("Fatorial de 0 é 0");
 		for (int i = 1; i <= 10; i++){
 			fatorial = fatorial * i;
 			System.out.println("Fatorial de " + i + " = " + fatorial);
-			
 		}
 
 	}
