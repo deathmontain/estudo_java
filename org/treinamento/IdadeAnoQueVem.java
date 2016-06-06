e853c34c26042055e8ba72f2c1510bdc6d27011c
Jônatas William <jonatas.santos@aedu.com>
2016-06-06 18:05:33 -0300

    Adicionado o exercicio Idade no ano que vem

---
+++ org/treinamento/IdadeAnoQueVem.java
@@ -0,0 +1,19 @@
+package org.treinamento;
+
+public class IdadeAnoQueVem {
+
+	public static void main(String[] args) {
+		int idade = 20;
+		
+		System.out.println(idade);
+		
+		int idadeAnoQueVem;
+		
+		idadeAnoQueVem = idade + 1;
+		
+	    System.out.println("Idade no ano que vem: " + idadeAnoQueVem);
+	    
+
+	}
+
+}
