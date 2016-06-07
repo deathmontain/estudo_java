c83f87aad56be64d577e1a2109540a11fb3de2ee
Jônatas William <jonatas.santos@aedu.com>
2016-06-06 21:50:56 -0300

    Adicionado o fatorial de 0.

--- org/treinamento/Fatorial.java
+++ org/treinamento/Fatorial.java
@@ -4,7 +4,7 @@ public class Fatorial {
 
 	public static void main(String[] args) {
 		int fatorial = 1;
-		System.out.println("Fatorial de 0 é 0");
+		System.out.println("Fatorial de 0 = 1");
 		for (int i = 1; i <= 10; i++){
 			fatorial = fatorial * i;
 			System.out.println("Fatorial de " + i + " = " + fatorial);
