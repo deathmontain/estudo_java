16027f34072eff8dbf899c61471dec12c932b880
Jônatas William <jonatas.santos@aedu.com>
2016-06-21 20:19:08 -0300

    Testando Exception ao tentar abrir um arquivo inexistente.

---
+++ org/treinamento/TestaException.java
@@ -0,0 +1,14 @@
+package org.treinamento;
+
+import java.io.FileNotFoundException;
+
+public class TestaException {
+	  public static void main(String[] args){
+		  try{
+			  new java.io.FileInputStream("arquivo.txt");
+		  }catch (FileNotFoundException e){
+			  //System.out.println("Arquivo não foi encontrado");
+			  e.printStackTrace();  
+		  }
+	  }
+}
